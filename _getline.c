98496baf48b4436e4f9c9f39629b89925869477c
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-11 19:37:53 +0100

    feat: add my own version of getline

--- _getline.c
+++ _getline.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *
+ * 
  *
  *
  *
@@ -10,20 +10,16 @@
 ssize_t _getline(char **lineptr, size_t *n, int fd)
 {
 	char *mem;
-	ssize_t n_read, i;
-	size_t bufsize, supply_count;
-	int loop_sig, new_line_sig;
+	ssize_t n_read, supply_count;
+	size_t bufsize, i;
 
 
 	bufsize = BUFSIZE;
 	supply_count = 0;
-	loop_sig = 1;
-	new_line_sig = 0;
 	mem = NULL;
-	n_read = 0;
 	i = 0;
 
-	if (*lineptr == NULL && *n == 0)
+	if (*lineptr == NULL || *n == 0)
 	{
 		mem = malloc_checked(sizeof(char) * bufsize);
 		*lineptr = mem;
@@ -36,23 +32,27 @@ ssize_t _getline(char **lineptr, size_t *n, int fd)
 	}
 
 
-	/* the readline function will read the line and store the characters in a
-	 * buffer and return the address to the buffer then the getline function
-	 * will copy the characters from the buffer used by the readline function
-	 * to store the intial characters it read. While the copy is happening the
-	 * count of the copied character will be maintained and checked against the
-	 * total size of the getline buffer. If count of copied character has the
-	 * limit of the getline buffer before the copy ends, a call to realloc is
-	 * made and the pointer returned assigned to the variable that previously
-	 * held the getline buffer address so the copy can pickup from where it
-	 * stopped.
+	/**
+	 * The read system call reads up to the new line character in the line
 	 */
 
 
 
-	while (loop_sig == 1)
+	while ((n_read = read(fd, mem + supply_count, bufsize - supply_count)) > 0)
 	{
+		supply_count += n_read;
 
+		/* check for newline character in the newly read data */
+		for (i = supply_count - n_read; i < supply_count; i++)
+		{
+			if (mem[i] == '\n')
+			{
+				mem[i] = '\0';
+				return (supply_count);
+			}
+		}
+
+		/* check if the buffer is full, and if so, reallocate it */
 		if (supply_count >= bufsize)
 		{
 			bufsize += BUFSIZE;
@@ -62,29 +62,10 @@ ssize_t _getline(char **lineptr, size_t *n, int fd)
 			*lineptr = mem;
 		}
 
-		n_read = read(fd, mem, bufsize);
-		if (n_read == -1)
-			perror("Error reading input");
-
-		while (i < n_read)
-		{
-			if (mem[i] == '\n')
-			{
-				printf("yes");
-				mem[i] = '\0';
-				new_line_sig = 1;
-				loop_sig = 0;
-			}
-			i++;
-		}
-
-		if (new_line_sig == 0 && n_read > 0)
-		{
-			mem[n_read] = '\0';
-			loop_sig = 0;
-		}
-		supply_count += n_read;
 	}
+
+	if (n_read == 0 && supply_count == 0)
+		return (-1);
 
 	return (supply_count);
 }
