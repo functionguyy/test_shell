0542601a467ae4e64cdd3cbb32420d1a199c2fac
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-15 20:04:22 +0100

    fix: add new function prototype

--- simple_shell/main.h
+++ simple_shell/main.h
@@ -44,5 +44,8 @@ size_t list_len(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int processExecute(char **line_arr);
+size_t print_list(const list_t *h);
+void freeArrayOfPtr(char **arr);
+char *_readLine(void);
 
 #endif /* MAIN_H_ */
