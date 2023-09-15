c434e115de5d7e5375b272875280e17a06e13d80
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-16 00:44:06 +0100

    feat: add files for utility functions

---
+++ simple_shell/utils.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+ssize_t isEmptyString(char *str, char *delimiter)
+{
+	/* declare variables */
+	char *stringPtr;
+
+
+	/* initialize variables */
+	stringPtr = str;
+
+	/* scan the string */
+	while (*stringPtr == *delimiter)
+		stringPtr++;
+
+	if (*stringPtr == '\0')
+		return (-1);
+
+	return (0);
+};
