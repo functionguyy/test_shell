5178640edec78a8f5d6fee474501df5fdd116e36
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-15 20:05:07 +0100

    fix: put readline function

--- simple_shell/prompt.c
+++ simple_shell/prompt.c
@@ -7,34 +7,36 @@
  */
 char *_prompt(void)
 {
-	ssize_t n_read;
-	static char *line_ptr;
-	size_t n;
+	/*ssize_t n_read; */
+	char *line_ptr;
+	/*size_t n;*/
 
 
-	line_ptr = NULL;
-	n = 0;
-	errno = 0;
+	/*line_ptr = NULL;*/
+	/* n = 0;*/
+	/*errno = 0;*/
 
 
 	printf("OMSH$ ");
 	fflush(stdout);
 
 	/* build a readline function */
-	n_read = getline(&line_ptr, &n, stdin);
+	line_ptr = _readLine();
+
+	/* n_read = getline(&line_ptr, &n, stdin);
 	if (n_read == -1 && errno != 0)
 	{
 		perror("getline failed");
 		free(line_ptr);
 		return (NULL);
-	}
+	}*/
 
 	/* executes when Ctrl+D is used to signal end-of-file */
-	if (n_read == -1 && errno == 0)
+	/*if (n_read == -1 && errno == 0)
 	{
 		free(line_ptr);
 		return (NULL);
-	}
+	}*/
 
 	/* n_write = write(STDOUT_FILENO, line_ptr, n_read); */
 
