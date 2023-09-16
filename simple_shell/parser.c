5317087645fe6040fdfef3f94fc0b4fca3f4fa52
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-16 12:29:32 +0100

    feat: add function that parses a line read from stdin

---
+++ simple_shell/parser.c
@@ -0,0 +1,63 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void overwriteNewlineByte(char *str)
+{
+	/* declare variables */
+	char *newlineStrPtr;
+	size_t idx;
+
+	/* initialize variables */
+	newlineStrPtr = str;
+	idx = 0;
+
+	/* get index of new line character */
+	while (newlineStrPtr[idx] != '\n')
+		idx++;
+
+	/* overwrite the newline character with a null-byte */
+	newlineStrPtr[idx] = '\0';
+}
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+char **parseLine(char *str, char *delimiter)
+{
+	/* declare variables */
+	char *fullStr, **ptrArr;
+	list_t *head;
+
+
+	/* initialize variables */
+	fullStr = str;
+	head = NULL;
+
+	/* confirm argument passed is not empty */
+	if (fullStr == NULL || *fullStr == '\0')
+		return (NULL);
+
+	/*remove new line character from line */
+	if (strchr(fullStr, '\n') != NULL)
+		overwriteNewlineByte(fullStr);
+
+	/* check that string contains atleast one non-delimiter character */
+	if (isEmptyString(fullStr, delimiter) == -1)
+		return (NULL);
+
+	head = createListOfLineTokens(fullStr, delimiter);
+	ptrArr = createArrayOfLineTokens(head);
+
+
+	return (ptrArr);
+}
