31e3338007169d342d4ca101b890330ce124792f
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-19 11:42:29 +0100

    feat: add function that prints the enviroment variables of the current process

--- simple_shell/_builtins.c
+++ simple_shell/_builtins.c
@@ -2,13 +2,23 @@
 
 
 /* implement exit */
-
-
-
-
-/* implement env */
-int env(void)
+int exit_prog(void)
 {
+	/* raise a signal */
+	/* so the program can perform some cleanup before exiting */
+
+	return (0);
+}
+/**
+ * print_env - prints the environment variables and values
+ *
+ * Return: 0 on success.Always successful.
+ */
+/* implement env */
+int print_env(void)
+{
+	/* implement env */
+
 	/* declare variables */
 	char **ep;
 
@@ -19,10 +29,9 @@ int env(void)
 	{
 
 		/* bring your printf function to use here */
-
-
+		printf("%s\n", *ep);
+		ep++;
 	}
 
-
-	exit(EXIT_SUCCESS);
+	return (0);
 }
