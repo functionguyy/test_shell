a6ed8457ca154a25da20786a27aecea07f686671
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-19 02:19:49 +0100

    feat: add function for interactive and non-interactive interface

--- simple_shell/prompt.c
+++ simple_shell/prompt.c
@@ -1,21 +1,13 @@
 #include "main.h"
 /**
+ * _prompt - print the shell's prompt and reads the input
  *
- *
- *
+ * Return: returns pointer to the input string or NULL
  *
  */
 char *_prompt(void)
 {
-	/*ssize_t n_read; */
 	char *line_ptr;
-	/*size_t n;*/
-
-
-	/*line_ptr = NULL;*/
-	/* n = 0;*/
-	/*errno = 0;*/
-
 
 	printf("OMSH$ ");
 	fflush(stdout);
@@ -23,26 +15,73 @@ char *_prompt(void)
 	/* build a readline function */
 	line_ptr = _readLine();
 
-	/* n_read = getline(&line_ptr, &n, stdin);
-	if (n_read == -1 && errno != 0)
-	{
-		perror("getline failed");
-		free(line_ptr);
-		return (NULL);
-	}*/
-
-	/* executes when Ctrl+D is used to signal end-of-file */
-	/*if (n_read == -1 && errno == 0)
-	{
-		free(line_ptr);
-		return (NULL);
-	}*/
-
-	/* n_write = write(STDOUT_FILENO, line_ptr, n_read); */
-
-
-	/*putchar('\n');*/
-	/*free(line_ptr);*/
 
 	return (line_ptr);
+}
+/**
+ * runNonInteractive - interface to execute commands passed to redirections
+ *
+ * Return:  always 0 on success. Always successful.
+ */
+/* put all the non-interactive code here */
+int runNonInteractive(void)
+{
+	char *inputLine, **cmdLineArr;
+	cmd_t *cmdData;
+
+	inputLine = _readLine();
+	if (inputLine == NULL)
+		return (0);
+	/* split input line and create array of words */
+	cmdLineArr = parseLine(inputLine, " ");
+	if (cmdLineArr == NULL)
+		return (0);
+	/* execute the command */
+	cmdData = searchCmd(cmdLineArr[0]);
+	if (cmdData == NULL)
+	{
+		free(cmdData);
+		freeArrayOfPtr(cmdLineArr);
+		return (0);
+	}
+	executeFunc(cmdData, cmdLineArr);
+
+	return (0);
+}
+/**
+ * runInteractive - interface to start up the terminal to input command after a
+ * prompt
+ *
+ * return: always 0 on success. Alway successful.
+ */
+
+int runInteractive(void)
+{
+	char *inputLine, **cmdLineArr;
+	cmd_t *cmdData;
+	size_t cmdCount;
+
+	cmdCount = 0;
+
+	while (1)
+	{
+		cmdCount++;
+		inputLine = _prompt();
+		if (inputLine == NULL)
+			break;
+		cmdLineArr = parseLine(inputLine, " ");
+		if (cmdLineArr == NULL)
+			continue;
+		cmdData = searchCmd(cmdLineArr[0]);
+		if (cmdData == NULL)
+		{
+			printf("./hsh: %lu: %s: not found\n", cmdCount, cmdLineArr[0]);
+			free(cmdData);
+			freeArrayOfPtr(cmdLineArr);
+			continue;
+		}
+
+		executeFunc(cmdData, cmdLineArr);
+	}
+	return (0);
 }
